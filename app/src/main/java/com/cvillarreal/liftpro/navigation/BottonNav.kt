89c41ae1b488dd3603479ba72dbdbd2dea613dac
Conor Villarreal <cdvillarreal@wisc.edu>
2023-03-30 17:35:53 -0500

    Running App

--- app/src/main/java/com/cvillarreal/liftpro/navigation/BottonNav.kt
+++ app/src/main/java/com/cvillarreal/liftpro/navigation/BottonNav.kt
@@ -6,13 +6,17 @@ import androidx.compose.material.BottomNavigationItem
 
 import androidx.compose.material.Text
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 
 @Composable
 fun BottomNav(navController: NavHostController, navItems: List<BottomNavScreens>) {
-    BottomNavigation {
+    // TODO: Switch to Material3 NavigationBar
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colorScheme.primaryContainer
+    ) {
         val currentRoute = navController.currentBackStackEntry?.destination?.route
         navItems.forEach {
             BottomNavigationItem(
@@ -23,8 +27,10 @@ fun BottomNav(navController: NavHostController, navItems: List<BottomNavScreens>
                     if (currentRoute != it.screen_route) {
                         navController.navigate(it.screen_route)
                     }
-                }
+                },
             )
         }
+        val backgroundColor = MaterialTheme.colorScheme.primaryContainer
     }
+
 }

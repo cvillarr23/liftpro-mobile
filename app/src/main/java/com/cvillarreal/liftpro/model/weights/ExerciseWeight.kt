75d32ac0b1283e24cc45272465ba15afc12a2f37
cvillarr23 <null>
2023-03-30 21:08:41 +0000

    🔄 created local 'app/src/main/java/com/cvillarreal/liftpro/model/' from remote 'src/main/kotlin/com/cvillarreal/liftpro/model/'

---
+++ app/src/main/java/com/cvillarreal/liftpro/model/weights/ExerciseWeight.kt
@@ -0,0 +1,30 @@
+package com.cvillarreal.liftpro.model.weights
+
+/**
+ * Represents a weight in kilograms
+ * @property totalWeight The weight in kilograms
+ * @constructor Creates a new weight
+ */
+//class ExerciseWeight(
+//    val totalWeight: Weight,
+//    val baseWeight: BaseWeight? = null
+//) {
+//
+//    /**
+//     * Converts the weight to pounds
+//     * @return The weight in pounds
+//     */
+//    fun toPounds(): Double {
+//        return totalWeight.toPounds()
+//    }
+//
+//    /**
+//     * Calculates the number of plates needed to reach the desired weight.
+//     * @return A list of plates needed to reach the desired weight on each side.
+//     */
+//    fun calculatePlates(): List<Weight>{
+//        TODO("Not yet implemented")
+//    }
+//
+//
+//}
